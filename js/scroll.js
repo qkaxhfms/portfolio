9568fabb00de960abf3850a93ab87e14b36c5321
peter <kwakms821@gmail.com>
2020-01-21 00:41:28 +0900

    change javascript

--- js/scroll.js
+++ js/scroll.js
@@ -9,16 +9,16 @@ $(document).ready(function(){
     }
     posArr.push(    $('#container>div').last().offset().top + $('#container>div').last().height()    )
 
-    $(window).on('scroll',function(){
-		var scroll = $(this).scrollTop();
+    // $(window).on('scroll',function(){
+	// 	var scroll = $(this).scrollTop();
 
-		for(var i=0; i<len; i++){
-			if(scroll >= posArr[i] && scroll < posArr[i+1]){
-				$('#gnb li').removeClass('on');
-				$('#gnb li').eq(i).addClass('on');
-			}
-		}
-    })
+	// 	for(var i=0; i<len; i++){
+	// 		if(scroll >= posArr[i] && scroll < posArr[i+1]){
+	// 			$('#gnb li').removeClass('on');
+	// 			$('#gnb li').eq(i).addClass('on');
+	// 		}
+	// 	}
+    // })
 
     $('#gnb li').on('click',function(){
         var i = $(this).index();
