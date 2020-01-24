93dd8f1b17be58aa1c048fe419085ad3b477ac75
peter <kwakms821@gmail.com>
2020-01-25 00:58:04 +0900

    intro change motion to javascript

--- js/motion.js
+++ js/motion.js
@@ -1,42 +1,64 @@
-$(document).ready(function(){
+const innerLetter1 = document.querySelector('.letter-1');
+const innerLetter2 = document.querySelector('.letter-2');
+var winW = document.documentElement.offsetWidth;
+console.log(winW);
+console.log(innerLetter1);
+console.log(innerLetter2);
 
-    resizeMotion();
-    $(window).on('resize',resizeMotion);
+// resizeMotion();
 
-    function resizeMotion(){
-        var winW = $(window).width(); 
 
-        if(winW > 940){        
-            delayIntro();  
-        }else{
-            $('.inner-letter.letter-1').addClass('change-svg change-stroke change-fill letter-move');
-        }
-        function delayIntro(){
-            setTimeout(function(){
-                $('.inner-letter.letter-1').addClass('change-svg');
-            },0);
-            setTimeout(function(){
-                $('.inner-letter.letter-1').addClass('change-stroke');
-            },1500);
-            setTimeout(function(){
-                $('.inner-letter.letter-1').addClass('change-fill');
-            },4500);
-            setTimeout(function(){
-                $('.inner-letter.letter-1').addClass('letter-move');
-            },4800);
-            setTimeout(function(){
-                $('.inner-letter.letter-2').addClass('change-svg');
-            },5000);
-            setTimeout(function(){
-                $('.inner-letter.letter-2').addClass('change-stroke');
-            },6500);
-            setTimeout(function(){
-                $('.inner-letter.letter-2').addClass('letter-move');
-            },9500);
-            setTimeout(function(){
-                $('.inner-letter.letter-2').addClass('change-fill');
-            },9800);
-        }
+// function resizeMotion(){
+
+// }
+
+// console.log(resizeMotion)
+
+function delayIntro(){
+    if(winW > 939){
+        setTimeout(function(){
+            innerLetter1.classList.add('change-svg');
+        },0);
+        setTimeout(function(){
+            innerLetter1.classList.add('change-stroke');
+        },1500);
+        setTimeout(function(){
+            innerLetter1.classList.add('change-fill');
+        },4500);
+        setTimeout(function(){
+            innerLetter1.classList.add('letter-move');
+        },4800);
+        setTimeout(function(){
+            innerLetter2.classList.add('change-svg');
+        },5000);
+        setTimeout(function(){
+            innerLetter2.classList.add('change-stroke');
+        },6500);
+        setTimeout(function(){
+            innerLetter2.classList.add('letter-move');
+        },9500);
+        setTimeout(function(){
+            innerLetter2.classList.add('change-fill');
+        },9800);    
+        clearTimeout(delayIntro);
+    }else{
+        innerLetter1.classList.add('change-svg','change-stroke', 'change-fill', 'letter-move');
+        innerLetter2.classList.add('change-svg', 'change-stroke', 'change-fill' ,'letter-move');
     }
+    
+}
 
-});
+// console.log(delayIntro)
+window.onload = function() {
+    delayIntro();
+}
+
+
+// window.onresize = function(){
+    
+//     if(winW > 940){
+//         delayIntro();
+//     } else {
+//         resizeMotion();
+//     }
+// }
---
+++ js/motion_jquery.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function(){
+
+    resizeMotion();
+    $(window).on('resize',resizeMotion);
+
+    function resizeMotion(){
+        var winW = $(window).width(); 
+
+        if(winW > 940){
+            delayIntro();  
+        }else{
+            $('.inner-letter.letter-1').addClass('change-svg change-stroke change-fill letter-move');
+            $('.inner-letter.letter-2').addClass('change-svg change-stroke change-fill letter-move');
+        }
+        function delayIntro(){
+            setTimeout(function(){
+                $('.inner-letter.letter-1').addClass('change-svg');
+            },0);
+            setTimeout(function(){
+                $('.inner-letter.letter-1').addClass('change-stroke');
+            },1500);
+            setTimeout(function(){
+                $('.inner-letter.letter-1').addClass('change-fill');
+            },4500);
+            setTimeout(function(){
+                $('.inner-letter.letter-1').addClass('letter-move');
+            },4800);
+            setTimeout(function(){
+                $('.inner-letter.letter-2').addClass('change-svg');
+            },5000);
+            setTimeout(function(){
+                $('.inner-letter.letter-2').addClass('change-stroke');
+            },6500);
+            setTimeout(function(){
+                $('.inner-letter.letter-2').addClass('letter-move');
+            },9500);
+            setTimeout(function(){
+                $('.inner-letter.letter-2').addClass('change-fill');
+            },9800);
+
+            clearTimeout(delayIntro);
+        }
+    }
+
+});
