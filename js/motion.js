3f04526744e84ef0e2cc6a308b8f53dc59387492
peter <kwakms821@gmail.com>
2020-01-26 03:08:02 +0900

    change aboutme section

--- js/motion.js
+++ js/motion.js
@@ -1,18 +1,6 @@
 const innerLetter1 = document.querySelector('.letter-1');
 const innerLetter2 = document.querySelector('.letter-2');
 var winW = document.documentElement.offsetWidth;
-console.log(winW);
-console.log(innerLetter1);
-console.log(innerLetter2);
-
-// resizeMotion();
-
-
-// function resizeMotion(){
-
-// }
-
-// console.log(resizeMotion)
 
 function delayIntro(){
     if(winW > 939){
@@ -52,13 +40,3 @@ function delayIntro(){
 window.onload = function() {
     delayIntro();
 }
-
-
-// window.onresize = function(){
-    
-//     if(winW > 940){
-//         delayIntro();
-//     } else {
-//         resizeMotion();
-//     }
-// }
--- js/scroll.js
+++ js/scroll.js
@@ -3,13 +3,36 @@ $(document).ready(function(){
     var speed = 1000;
     var posArr = [];
     var len = $('#container>div').length;
+	var base_line = -300;
     var mob_line = 80;
+
+	$('.content-about>div').each(function(index,item){
+		// console.log(item);
+		// console.log(index);
+		$(item).css({'transition-delay':	(0.2*index)+'s'	});
+	});
+
+
 
     for(var i=0; i<len; i++){
         posArr.push(    $('#container>div').eq(i).offset().top    );
     }
     posArr.push(    $('#container>div').last().offset().top + $('#container>div').last().height()    )
 
+	$(window).on('scroll',function(){
+		var scroll = $(this).scrollTop();
+
+		for(var i=0; i<len; i++){
+			// if(scroll >= posArr[i]+base && scroll < posArr[i+1]+base){
+			if(scroll >= posArr[i]+base_line && scroll < posArr[i+1]+base_line){
+
+				$('#container>div').removeClass('on');
+				$('#container>div').eq(i).addClass('on');
+			}
+		}
+
+
+	});
     //gnb
     $('#gnb li').on('click',function(){
         var i = $(this).index();
