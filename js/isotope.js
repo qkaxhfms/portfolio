0bcfa8191f65d07c60e7adcad3b529603aaa9322
peter <kwakms821@gmail.com>
2020-01-30 23:52:35 +0900

    change isotope.js

--- js/isotope.js
+++ js/isotope.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){	
-	
+	//item-wrapper
 	var $grid = $('.list-item-wrapper').isotope({
 		itemSelector : '.list-item',
 		columnWidth : '.list-item',
@@ -7,7 +7,7 @@ $(document).ready(function(){
 		transitionDuration : '0.8s'
 	});
 
-
+	//filter function
 	$('.filter a').on('click',function(e){
 		e.preventDefault();
 		var sort = $(this).attr('href');
