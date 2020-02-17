3e1cf88afe6111c2ff4ced4a4dfea1967471a5c4
unknown <kwakms821@daum.net>
2020-02-18 01:32:22 +0900

    add layout

--- pages/sample/js/common.js
+++ pages/sample/js/common.js
@@ -1,15 +1,24 @@
-const winW = document.documentElement.offsetWidth;
-const body = document.getElementsByTagName("BODY");
+const body = document.getElementsByTagName("body");
 const header = document.getElementsByTagName("header");
 //   header.background = "#f3f3f3";
 // this.console.log(winW);
 
 function layoutAttr() {
+  let winW = document.documentElement.offsetWidth;
+  //   console.error(winW);
   if (winW > 939) {
-    body.classList.add("pc");
+    body[0].classList.add("pc");
+    body[0].classList.remove("mobile");
+  } else {
+    body[0].classList.add("mobile");
+    body[0].classList.remove("pc");
   }
 }
 
 window.onload = function() {
+  layoutAttr();
+};
+
+window.onresize = function() {
   layoutAttr();
 };
