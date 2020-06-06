5de620b0418bccf980a3ea3cb3d2d4cdd101d33d
unknown <kwakms821@daum.net>
2020-06-06 23:11:12 +0900

    hotfix js

--- pages/instagram/js/common.js
+++ pages/instagram/js/common.js
@@ -1,28 +1,35 @@
-$(document).ready(function(){
-
-    $('.filter a').on('click',function(e){
-        e.preventDefault();
-        var sort = $(this).attr('href');
-        $grid.isotope({filter:sort});
-    })
+$(document).ready(function () {
     var token = "IGQVJVQWUxdXVsdW45LUpMZATZA6SzFkWjRiWlNkWnV4VWhMQzFoNHZAyZAzNFWnQ0SXZAEY1p1OTVDblkzTHBkdHBmSzEwRTJoRlhaQWFoSnlONzNndDZAIXzY3TV9nYU8wMS1OZAHhFU1V3";
 
     $.ajax({
         url: "https://graph.instagram.com/me/media?access_token=" + token + "&fields=id,caption,media_type,media_url,thumbnail_url,permalink",
-        dataType: "jsonp",// CORS 
-        complete: function(){
-            var $grid = $('.container').isotope({
-                itemSelector:'.item',
-                columnWidth:'.item',
-                percentPosition:true,
-                transitionDuration:'0.8s'
+        dataType: "jsonp",
+        complete: function () {
+            var $grid = $(".container").isotope({
+                itemSelector: ".item",
+                conlumnwidth: ".item",
+                percentPosition: true,
+                transitionDuration: "0.5s"
+            });
+
+            /* filtering */
+            $(".filter").on("click", "li .category", function (e) {
+                e.preventDefault();
+                var dataCategory = $(this).attr("data-category");
+                $grid.isotope({
+                    filter: function () {
+                        var hashTag = $(this).find("a").text();
+                        return hashTag.match(dataCategory);
+                    }
+                });
             });
         }
     }).done(function (data) {
         console.log(data);
         window.data = data;
         var list = "";
-        $.each(data.data, function (i, v) { // each문 반복문 돌림 -> data.data i-> index , v -> value
+
+        $.each(data.data, function (i, v) {
             list += "<div class='item'>";
             list += "<div class='instagram-box'>";
             list += "<a href='" + v.permalink + "' target='_blank' style='background:url(" + v.media_url + ") no-repeat center center;''>";
@@ -31,8 +38,28 @@ $(document).ready(function(){
             list += "</div>";
             list += "</div>";
         });
+
         $('.container').append(list);
+
+        /* range random value */
+        function random(min, max) {
+            var randVal = Math.floor(Math.random() * (max - min + 1)) + min;
+            return randVal;
+        }
+
+        $(".instagram-box").map(function (i, v) {
+            $(v).find("a").css("height", random(150, 350) + "px");
+        });
+
     }).fail(function () {
         console.log("fail");
     });
-})
+
+    /* create category */
+    $("#btn").click(function (e) {
+        e.preventDefault();
+        var txtVal = $(this).siblings("#addTxt").val();
+        var catagoryBtn = $("<li><a class='category' href='#' data-category='." + txtVal + "'>#" + txtVal + "</a></li>");
+        $(".filter").append(catagoryBtn);
+    });
+});
