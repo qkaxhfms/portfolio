7567a036715ac44171255e6d1b7d347cedb01a24
unknown <kwakms821@daum.net>
2020-07-29 01:20:24 +0900

    add scroll top button

--- js/common.js
+++ js/common.js
@@ -11,4 +11,22 @@ window.onload = () => {
         $('.listSlide').stop().slideToggle(200);
     })
 
+
+
+    $(window).on('scroll',function(){
+        var scroll = $(this).scrollTop();
+
+        if(scroll > 1000){
+            console.log("dsdfsd");
+            $('#btnTop').addClass("on");
+        }else{
+            $('#btnTop').removeClass("on");
+        }
+
+        $('#btnTop').on('click',function(){
+            $('html,body').stop().animate({ scrollTop: 0},220);
+        });
+    });
+
 }
+
