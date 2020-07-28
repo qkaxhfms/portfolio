ab673c581baa363790a279722428cf2296cfc797
unknown <kwakms821@daum.net>
2020-07-28 23:16:32 +0900

    change index

--- js/common.js
+++ js/common.js
@@ -1,7 +1,16 @@
 window.onload = () => {
-    document.getElementById("btnSwitch").addEventListener("click", logoClick);
+
+    const btnSwitch = document.querySelector("#btnSwitch");
+    // const btnMore = document.querySelector(".btnMore");
+
+    btnSwitch.addEventListener("click", logoClick);
+    // btnMore.addEventListener("click", btnClick);
+
     function logoClick(){
         document.querySelector("body").classList.toggle("change");
     }
-    
+
+    // function btnClick(){
+    //     document.querySelector(".listCon").classList.toggle("on");
+    // }
 }
