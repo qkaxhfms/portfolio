367b9aac7e4d8ee6b3014eb4f648984f01b13dab
unknown <kwakms821@daum.net>
2020-07-29 00:31:09 +0900

    change layout

--- js/common.js
+++ js/common.js
@@ -1,16 +1,14 @@
 window.onload = () => {
 
     const btnSwitch = document.querySelector("#btnSwitch");
-    // const btnMore = document.querySelector(".btnMore");
-
     btnSwitch.addEventListener("click", logoClick);
-    // btnMore.addEventListener("click", btnClick);
 
     function logoClick(){
         document.querySelector("body").classList.toggle("change");
     }
 
-    // function btnClick(){
-    //     document.querySelector(".listCon").classList.toggle("on");
-    // }
+    $('#btnMore').on('click',function(){
+        $('.listSlide').stop().slideToggle(200);
+    })
+
 }
